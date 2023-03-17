ff6c229355cf00c9c15138edf46bf32105b56c1e
Ben Cady <ben@cadyben.com>
2023-03-17 04:16:32 -0700

    Initial Commit

---
+++ SirTalkaLot/SirTalkaLotApp.swift
@@ -0,0 +1,17 @@
+//
+//  SirTalkaLotApp.swift
+//  SirTalkaLot
+//
+//  Created by Ben Cady on 3/17/23.
+//
+
+import SwiftUI
+
+@main
+struct SirTalkaLotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
